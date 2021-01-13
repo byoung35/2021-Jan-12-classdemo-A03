a27eecc2d8365d6ac4ee5f262bbee02d095f8054
byoung35 <byoung35@nait.ca>
2021-01-13 10:08:11 -0700

    My First Program update
    Changes made to my first program adding steps

---
+++ My First Program Solution/My First Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace My_First_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This program shows you how to withdraw money from an ATM in 7 steps.
+            Console.WriteLine("How to use an ATM to withdraw money");
+            Console.WriteLine("Step 1: \t Insert debit card into the card slot on the ATM");
+            Console.WriteLine("Step 2: \t Enter your debit card PIN");
+            Console.WriteLine("Step 3: \t Select the account to withdraw money from");
+            Console.WriteLine("Step 4: \t Select the amount of money to withdraw");
+            Console.WriteLine("Step 5: \t Confirm there are no further transactions");
+            Console.WriteLine("Step 6: \t Collect your money from the money slot");
+            Console.WriteLine("Step 7: \t Collect your debit card from the card slot");
+        }
+    }
+}
