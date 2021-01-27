ac7cf3f52688419b795c8161a110ba5adc3e309d
byoung35 <byoung35@nait.ca>
2021-01-27 11:25:59 -0700

    Temperature exercise
    example exercise using if else statements.

--- WeatherSol/Weather/Program.cs
+++ WeatherSol/Weather/Program.cs
@@ -6,7 +6,52 @@ namespace Weather
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int FahrenheitTemp = 75;
+            int CelsiusTemp = 0;
+
+            Console.Write("Enter a Fahrenheit temperature: ");
+            FahrenheitTemp = int.Parse(Console.ReadLine());
+            
+
+            CelsiusTemp = (FahrenheitTemp - 32) * 5 / 9;
+
+            if (CelsiusTemp < 0)
+            {
+                Console.WriteLine("It's freezing outside.");
+
+            }
+            else if(CelsiusTemp < 15)
+            {
+                Console.WriteLine("Wear a jacket.");
+            }
+            else if(CelsiusTemp < 30)
+            {
+                Console.WriteLine("It's a lovely day.");
+            }
+            else
+            {
+                Console.WriteLine("It's finally summer.");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
